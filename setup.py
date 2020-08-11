d900531699cd3c2bf284b1500dd66b2595bf1e92
Joshua Haas <haas.josh.a@gmail.com>
2020-08-10 23:24:04 -0400

    add ansp aliased command

--- setup.py
+++ setup.py
@@ -2,29 +2,37 @@
 
 import os
 
-MY_DIR = os.path.dirname(os.path.realpath(__file__))
+GIT_DIR = os.path.dirname(os.path.realpath(__file__))
+HOME_DIR = os.path.expanduser('~')
 
-HOME_DIR = os.path.expanduser('~/.ansible')
+ANS_DIR = os.path.expanduser('~/.ansible')
 CFG_FILE = 'ansible.cfg'
-HOME_DIR_FILE = os.path.join(HOME_DIR, CFG_FILE)
-HOME_FILE = os.path.expanduser('~/.' + CFG_FILE)
+ANS_DIR_FILE = os.path.join(ANS_DIR, CFG_FILE)
+ANS_FILE = os.path.expanduser('~/.' + CFG_FILE)
+SCRIPT = 'bin/ansp'
 
 TASKS = {}
 TASKS['linkdir'] = {
-  'func': os.symlink,
-  'args': [MY_DIR, HOME_DIR],
+  'args': [GIT_DIR, ANS_DIR],
 }
 TASKS['linkhomecfg'] = {
-  'func': os.symlink,
-  'args': [HOME_DIR_FILE, HOME_FILE],
-} 
+  'args': [ANS_DIR_FILE, ANS_FILE],
+}
+TASKS['linkscript'] = {
+  'args': [
+    os.path.join(GIT_DIR, SCRIPT),
+    os.path.join(HOME_DIR, SCRIPT),
+  ]
+}
 
 def main(tasks):
 
   for task in tasks.values():
     make(**task)
 
-def make(func, args=None, kwargs=None):
+  print('run: "sudo -H pip3 install ansible"')
+
+def make(func=os.symlink, args=None, kwargs=None):
 
   args = args or []
   kwargs = kwargs or {}
